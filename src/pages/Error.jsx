e0e446697b6d1b33633dc90ca4ee9fe124a09dfc
Kati Remo <kati.remo@ambientia.fi>
2023-04-20 11:42:38 +0300

    create error messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,29 @@
+import { Link, useNavigate, useRouteError } from "react-router-dom"
+
+// library
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+
 const Error = () => {
+  const error = useRouteError()
+  const navigate = useNavigate();
+
     return (
-      <div>
-        Error
+      <div className="error">
+        <h1>Uh oh! We have a problem!</h1>
+        <p> { error.message || error.statusText } </p>
+        <div className="flex-md">
+          <button
+          className="btn btn--dark"
+          onClick={() => navigate(-1)}
+          >
+            <ArrowUturnLeftIcon width={20} />
+            <span> Go back</span>
+          </button>
+          <Link to="/" className="btn btn--dark">
+            <HomeIcon width={20}/>
+            <span> Back home</span>
+          </Link>
+        </div>
       </div>
     )
   }
