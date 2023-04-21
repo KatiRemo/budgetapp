aa7e10e9b7de79a07ef66ef49c0e55f8fa431e13
Kati Remo <kati.remo@ambientia.fi>
2023-04-21 09:32:07 +0300

    add helper function to create expense

--- src/helpers.js
+++ src/helpers.js
@@ -26,7 +26,25 @@ export const createBudget = ({
     amount: +amount,
     color: generateRandomColor()
   }
+  
   const existingBudgets = fetchData("budgets") ?? []
   return localStorage.setItem("budgets", 
     JSON.stringify([...existingBudgets, newItem]))
+}
+
+// create a budget
+export const createExpense = ({
+  name, amount, budgetId
+}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId
+  }
+  
+  const existingExpenses = fetchData("expenses") ?? []
+  return localStorage.setItem("expenses", 
+    JSON.stringify([...existingExpenses, newItem]))
 }
