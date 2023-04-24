e095f0b23d70eda30bdfa51ad9da50619dc75b0c
Kati Remo <kati.remo@ambientia.fi>
2023-04-24 08:24:26 +0300

    create expensesAction to expensesPage and import it to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // routes
 import Dashboard, { dashboardAction, dashboardLoader } from './pages/Dashboard';
 import Error from './pages/Error'
-import ExpensesPage, { expensesLoader } from './pages/ExpensesPage';
+import ExpensesPage, { expensesAction, expensesLoader } from './pages/ExpensesPage';
 
 // actions
 import { logoutAction } from './actions/logout';
@@ -37,6 +37,7 @@ const router = createBrowserRouter([
         path: "expenses",
         element: <ExpensesPage />,
         loader: expensesLoader,
+        action: expensesAction
       },
       {
         path: "logout",
--- src/pages/ExpensesPage.jsx
+++ src/pages/ExpensesPage.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
-import { fetchData } from '../helpers'
+import { deleteItem, fetchData } from '../helpers'
 import Table from '../components/Table'
+import { toast } from 'react-toastify'
 
 // loader
-export function expensesLoader() {
-    const expenses = fetchData("expenses")
+export async function expensesLoader() {
+    const expenses = await fetchData("expenses")
     return { expenses }
   }
+
+// action
+export async function expensesAction({request}) {
+  const data = await request.formData();
+  const { _action, ...values} = Object.fromEntries(data);
+
+  if(_action === "deleteExpense") {
+    try {
+     // delete an expense
+      deleteItem ({
+        key: "expenses",
+        id: values.expenseId,
+      });
+      return toast.success("Expense deleted")
+    } catch(e) {
+      throw new Error ("There was a problem, expense could not be deleted")
+    }
+  }
+}
 
 const ExpensesPage = () => {
     const { expenses } = useLoaderData()
