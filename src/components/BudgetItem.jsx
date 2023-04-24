c873e8c70013127999b83d380b1617a474751d6d
Kati Remo <kati.remo@ambientia.fi>
2023-04-24 13:07:20 +0300

    update BudgetPage and BudgetItem for deleting budgets

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,6 +1,8 @@
+import { Form, Link } from "react-router-dom";
 import { calculateSpentByBudget, formatCurrency, formatPercentage } from "../helpers";
+import { BanknotesIcon, TrashIcon } from '@heroicons/react/24/outline'
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
     const { id, name, amount, color } = budget;
     const spent = calculateSpentByBudget(id)
 
@@ -22,6 +24,35 @@ const BudgetItem = ({ budget }) => {
                 <small>{formatCurrency(spent)} spent</small>
                 <small>{formatCurrency(amount - spent)} remaining</small>
             </div>
+            {
+                showDelete ? (
+                    <div className="flex-sm">
+                        <Form
+                        method="post"
+                        action="delete"
+                        onSubmit={(event) => {
+                            if(!window.confirm("Do you want to permanently delete the budget?")){
+                                event.preventDefault();
+                            }
+                        }}
+                        >
+                            <button type="submit" className="btn">
+                                <span>Delete Budget</span>
+                                <TrashIcon width={20} />
+                            </button>
+                        </Form>
+                    </div>
+                ) : (
+                    <div className="flex-sm">
+                        <Link
+                        to={`/budget/${id}`}
+                        className="btn">
+                            <span>View Details</span>
+                            <BanknotesIcon width={20} />
+                        </Link>
+                    </div>
+                )
+            }
         </div>
     )
 }
